5bf1b4c6f178aa60b75dd3805b5d265236f8ff59
Antoine.Del <antoine.delaplace@gmail.com>
2018-11-11 22:34:29 +0100

    angular tutorial

---
+++ angular-tour-of-heroes/out-tsc/app/app/component/hero-detail.component.js
@@ -0,0 +1,53 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+import { Component, Input } from '@angular/core';
+import { Hero } from '../entity/hero';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from '../service/hero.service';
+import 'rxjs/add/operator/switchMap';
+var HeroDetailComponent = (function () {
+    function HeroDetailComponent(heroService, route, location) {
+        this.heroService = heroService;
+        this.route = route;
+        this.location = location;
+    }
+    HeroDetailComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        this.route.params
+            .switchMap(function (params) { return _this.heroService.getHero(+params['id']); })
+            .subscribe(function (hero) { return _this.hero = hero; });
+    };
+    HeroDetailComponent.prototype.goBack = function () {
+        this.location.back();
+    };
+    HeroDetailComponent.prototype.save = function () {
+        var _this = this;
+        this.heroService.update(this.hero)
+            .then(function () { return _this.goBack(); });
+    };
+    return HeroDetailComponent;
+}());
+__decorate([
+    Input(),
+    __metadata("design:type", Hero)
+], HeroDetailComponent.prototype, "hero", void 0);
+HeroDetailComponent = __decorate([
+    Component({
+        selector: 'hero-detail',
+        templateUrl: '../template/hero-detail.html',
+        styleUrls: ['../css/hero-detail.css']
+    }),
+    __metadata("design:paramtypes", [HeroService,
+        ActivatedRoute,
+        Location])
+], HeroDetailComponent);
+export { HeroDetailComponent };
+//# sourceMappingURL=hero-detail.component.js.map
